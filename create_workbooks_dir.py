e6f5d15b9ab67980af07d4e200866bf6a428910e
noakup <noakup@gmail.com>
2020-06-07 22:40:50 +0300

    added readme files to Alerts and Queries folders

--- create_workbooks_dir.py
+++ create_workbooks_dir.py
@@ -24,27 +24,22 @@ mode = 0o666
 for listedItem in os.listdir('.'):
     if os.path.isdir(listedItem):
         print('working on: ', listedItem)
-        WBAlertsPath = os.path.join(listedItem, "Workbooks", "Alerts")
-        if os.path.isdir(WBAlertsPath):
-            print("Directory '%s' exists, checking if empty..." %WBAlertsPath)
-            if not os.listdir(WBAlertsPath):
-                print("folder is empty, removing")
-                os.removedirs(WBAlertsPath)
-            else:
-                print("folder not empty")   
+        queriesPath = os.path.join(listedItem, "Queries")
+        alertsPath = os.path.join(listedItem, "Alerts")
+        if os.path.isdir(queriesPath):
+            if not os.listdir(queriesPath):
+                print("Directory '%s' exists and empty, writing readme file" % queriesPath)
+                with open(os.path.join(queriesPath, "README"), "w") as file:
+                    file.write("Put Log Analytics queries in this folder")
         else:
-            print("Directory %s doesn't exist" %WBAlertsPath)
-
-for listedItem in os.listdir('.'):
-    if os.path.isdir(listedItem):
-        print('working on: ', listedItem)
-        WBQueriesPath = os.path.join(listedItem, "Workbooks", "Queries")
-        if os.path.isdir(WBQueriesPath):
-            print("Directory '%s' exists, checking if empty..." %WBQueriesPath)
-            if not os.listdir(WBQueriesPath):
-                print("folder is empty, removing")
-                os.removedirs(WBQueriesPath)
-            else:
-                print("folder not empty")   
+            os.mkdir(queriesPath, mode) 
+            print("Directory '%s' created" % queriesPath)
+        
+        if os.path.isdir(alertsPath):
+            if not os.listdir(alertsPath):
+                print("Directory '%s' exists and empty, writing readme file" % alertsPath)
+                with open(os.path.join(alertsPath, "README"), "w") as file:
+                    file.write("Put alerts in this folder")
         else:
-            print("Directory %s doesn't exist" %WBQueriesPath)
+            os.mkdir(alertsPath, mode) 
+            print("Directory '%s' created" % alertsPath)
